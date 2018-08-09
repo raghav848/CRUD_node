1a88639dbea98a3f4ff4cbe10b6050081ac23292
droid <raghav.sharma848@gmail.com>
2018-08-09 18:27:07 +0530

    4th

--- Students/Students.controller.js
+++ Students/Students.controller.js
@@ -11,7 +11,6 @@ router.get('/add_students',function(req,res){
 }); 
 
 router.post('/add_students',function(req,res){
-    console.log(req.body.fname+'hi' );
     student.create({
         fname:req.body.fname,
         lname: req.body.lname,
@@ -26,6 +25,35 @@ router.get('/view_Students',function(req,res){
     student.find({},function(err,result){
         if(err) throw err;
         res.render('viewStudents',{data:result});
+    });
+});
+
+router.get('/delete_students/:id',function(req,res){
+    student.findByIdAndRemove(req.params.id,function(err,result){
+        if(err) throw err;
+        res.redirect('/students/view_students');
+    });
+});
+
+router.get('/update_students/:id',function(req,res){
+    student.findById(req.params.id
+    ,function(err,result){
+        if(err) throw err;
+        console.log(result);
+        res.render('addStudents',{data:result});
+    });
+});
+
+router.post('/update_students/:id',function(req,res){
+    student.findByIdAndUpdate(req.params.id,{
+        $set:{
+            fname:req.body.fname,
+            lname: req.body.lname,
+            rollNumber: req.body.rollNumber
+        }
+    },function(err,result){
+        if(err) throw err;
+        res.redirect('/students/view_students');
     });
 });
 
